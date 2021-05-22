599c6a7abab374ba28ad9944e019d5e06fbefa3b
kenta-0126 <garake_yarou@icloud.com>
2021-05-22 02:38:55 +0000

    [ADD]cssでリンクカーソルの色を重なったときに黒くなるように変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render :new
+      render :index
     end
     
   end
@@ -29,18 +29,18 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
        flash[:notice] = "Book was successfully updated."
-       redirect_to book_path(book.id)
+       redirect_to book_path(@book.id)
     else
       render :edit
     end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    if book.destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
       flash[:notice] = "Book was successfully destroyed."
        redirect_to books_path
     else
